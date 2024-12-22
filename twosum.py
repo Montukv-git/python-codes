58096c35514d1fbc420e3e10e2f905e593401c5d
Montukv-git <Montukeshwar20@gmail.com>
2024-12-22 17:13:15 +0530

    optimal solution with dict

--- twosum.py
+++ twosum.py
@@ -7,7 +7,10 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 """
 
-#My Solution 1      -       Brute force
+nums = [2,7,11,15]
+target = 13
+
+#Solution 1      -       Brute force        O(n^2)
 
 def twosum(nums, target):
     for i in range(0,len(nums)):
@@ -15,9 +18,22 @@ def twosum(nums, target):
             if nums[i]+nums[j]  ==  target:
                 return [i,j]
             
-    return "No Answer"
+    return []
 
-nums = [2,7,11,15]
-target = 13
+#print(twosum(nums, target))
 
-print(twosum(nums, target))
+#Solution 2         -   optimal solution with dict with time complexcity of O(1)
+
+def twosumoptimal(nums, target):
+    temp_dict =   {}
+    for i,num in enumerate(nums):
+        rem =   target - num
+        if rem in temp_dict:
+            return[temp_dict[rem],i]
+        
+        temp_dict[num]   =   i
+            
+    return []
+
+print(twosumoptimal(nums, target))
+
