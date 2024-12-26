4d4c86264b098d523da0ac1eb5a2ba7e3ff487d2
Montukv-git <Montukeshwar20@gmail.com>
2024-12-26 15:06:42 +0530

    rotate array easy

--- rotate_array_k.py
+++ rotate_array_k.py
@@ -7,8 +7,26 @@ rotate 2 steps to the right: [6,7,1,2,3,4,5]
 rotate 3 steps to the right: [5,6,7,1,2,3,4]
 """
 
-def rotate_array(nums,k):
-    return nums
+#solution
 
+def rotate(nums, k):
+    n = len(nums)
+    k=k%n
+    nums[:] = nums[-k:]+nums[:-k] 
 
-print(rotate_array(nums=[1,2,3,4,5,6,7],k=3))
+test_cases = [
+    ([1, 2, 3, 4, 5, 6, 7], 3, [5, 6, 7, 1, 2, 3, 4]),
+    ([1, 2, 3, 4], 4, [1, 2, 3, 4]),
+    ([10], 100, [10]),
+    ([1, 2, 3, 4], 0, [1, 2, 3, 4]),
+    ([1, 2, 3, 4], 6, [3, 4, 1, 2]),
+    ([1, 1, 1, 2, 2, 3], 2, [2, 3, 1, 1, 1, 2]),
+    ([5, 5, 5, 5], 3, [5, 5, 5, 5]),
+    ([1, 2, 3], 1, [3, 1, 2])
+]
+
+# testing multiple cases
+for i, (nums, k, expected) in enumerate(test_cases):
+    original = nums[:]
+    rotate(nums, k)
+    print(f"Test {i + 1}: {'Pass' if nums == expected else 'Fail'} (Input: {original}, k={k}, Output: {nums}, Expected: {expected})")
