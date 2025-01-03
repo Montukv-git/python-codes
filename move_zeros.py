7faf3cf18bd99b46a4fe99fb31d96f47603bf751
Montukv-git <Montukeshwar20@gmail.com>
2025-01-03 21:04:42 +0530

    fix

--- move_zeros.py
+++ move_zeros.py
@@ -4,14 +4,18 @@ Output: [1,3,12,0,0]
 """
 
 def move_zeros(nums):
-    for i,num in enumerate(nums):
-        if num == 0:
-            nums.pop(i)
-            nums.append(0)
+    non_zero_index = 0
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[non_zero_index] = nums[i]
+            non_zero_index += 1
+    for i in range(non_zero_index, len(nums)):
+        nums[i] = 0
     return nums
+
  
 print(move_zeros([0,1,0,3,12]))
-print(move_zeros([0,1,0,0,0]))
+print(move_zeros([0,0,1]))
 print(move_zeros([0]))
 print(move_zeros([]))
 
