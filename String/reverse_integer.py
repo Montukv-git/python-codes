46f38db6394a39fba067b0c04c963c8f1827d923
Montukv-git <Montukeshwar20@gmail.com>
2025-01-07 20:06:21 +0530

    edge case fix for space

--- String/reverse_integer.py
+++ String/reverse_integer.py
@@ -9,7 +9,7 @@ Output: 21
 
 def reverse_integer(x):
     rev = int(str(abs(x))[::-1]) * (1 if x > 0 else -1)
-    return rev 
+    return rev if -(2**31) <= rev <= (2**31 - 1) else 0
 
 x = 123
 print(reverse_integer(x))
